730df003809cfd4fc764ced6aa3ee5e7ff613209
Michał K <michalmatwijow@gmail.com>
2026-05-19 15:42:57 +0200

    about

--- src/pages/OMnie.jsx
+++ src/pages/OMnie.jsx
@@ -1,29 +1,18 @@
 export default function AboutPage() {
   return (
-    <section className="w-full min-h-screen bg-[#f5f5f5] py-20 px-6">
+    <section className="w-full min-h-screen py-20 px-6">
       <div className="max-w-6xl mx-auto grid lg:grid-cols-1 gap-12 text-left">
         <div>
 
           <p className="text-gray-600 text-lg leading-relaxed mb-6" >
             Cześć!</p>
-            
-            
-            <p className="text-gray-600 text-lg leading-relaxed mb-6"> Mam 12 lat doświadczenia w projektowaniu materiałów drukowanych i digitalowych (e-commerce, kampanie outdoorowe, skład publikacji).
-            W 2013 roku ukończyłem studia magisterskie na kierunku Grafika na Wydziale Sztuk Pięknych UMK w Toruniu, uzyskując tytuł magistra sztuki. W swoich projektach stawiam na czytelność i estetykę.
+
+
+          <p className="text-gray-600 text-lg leading-relaxed mb-6"> Mam 12 lat doświadczenia w projektowaniu materiałów drukowanych i digitalowych (e-commerce, kampanie outdoorowe, skład publikacji). Tworzę również responsywne strony internetowe oparte na React. W 2013 roku ukończyłem studia magisterskie na kierunku Grafika na Wydziale Sztuk Pięknych UMK w Toruniu, uzyskując tytuł magistra sztuki.
+
+          </p><p className="text-gray-600 text-lg leading-relaxed mb-6">W swoich projektach stawiam na czytelność, funkcjonalność i estetykę. Potrafię pracować zgodnie z założeniami brandbooka oraz dostosowywać projekty do wymagań i charakteru marki.
           </p>
 
-          <p className="text-gray-600 text-lg leading-relaxed mb-6">
-            Tworzę materiały do druku i internetu — od brandingów,
-            plakatów i social media, po responsywne strony internetowe i
-            aplikacje oparte o React. W swoich projektach stawiam na
-            przejrzystość, nowoczesny wygląd oraz intuicyjny UX.
-          </p>
-
-          <p className="text-gray-600 text-lg leading-relaxed mb-8">
-            Lubię łączyć design z technologią, projektując rozwiązania,
-            które nie tylko dobrze wyglądają, ale również działają szybko i
-            wygodnie na każdym urządzeniu.
-          </p>
 
         </div>
 
